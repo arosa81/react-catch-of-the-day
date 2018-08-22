fc9f7d8c9c6ee0a002e79959b82e7b073d3be894
Alexander Rosa <alex.j.rosa@gmail.com>
2018-08-21 19:47:57 -0600

    Func: complete basic routing

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div>Not Found</div>
+  <div>
+    <h2>Not Found</h2>
+  </div>
 )
 
 export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
-import App from './components/App';
 import "./css/style.css"
 
 ReactDOM.render(
