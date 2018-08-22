ccd71b8f0bdb75be50ae07f2e543ed43cac5c3d6
Alexander Rosa <alex.j.rosa@gmail.com>
2018-08-21 22:13:04 -0600

    Func: Handling form event and reroute

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,34 @@
 import React, { Component, Fragment } from 'react';
 
 class StorePicker extends Component {
+  state = {
+    storeName: '',
+  }
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.state.storeName);
+    this.props.history.push(`/store/${this.state.storeName}`);
+  }
+
+  handleStoreNameChange = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({storeName: value}));
+  }
+  
   render() {
     return (
       <Fragment>
-        <form action="" className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            value={this.state.storeName}
+            onChange={this.handleStoreNameChange}
+          />
           <button type="submit">Visit Store</button>
         </form>
       </Fragment>
